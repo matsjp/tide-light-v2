59be2ea94ae4878ba85c9a64fec1053ba48d38a2
Mats Johan Pedersen <MatsPedersen@protonmail.com>
2019-08-10 16:08:03 +0200

    Changed off color to blue

--- tidelight/TideLightLedStrip.py
+++ tidelight/TideLightLedStrip.py
@@ -12,6 +12,9 @@ class TideLightLedStrip(PixelStrip):
     
     def _Off(self):
         return Color(0, 0, 0)
+
+    def _Blue(self):
+        return Color(0, 0, 255)
     
     
     def update_tide_leds(self, led_number, direction):
@@ -21,12 +24,12 @@ class TideLightLedStrip(PixelStrip):
             for i in range(1, led_number + 1):
                 self.setPixelColor(i, self._Green())
             for i in range(led_number + 1, self.numPixels() - 1):
-                self.setPixelColor(i, self._Off())
+                self.setPixelColor(i, self._Blue())
         else:
             self.setPixelColor(0, self._Red())
             self.setPixelColor(self.numPixels() - 1, self._Off())
             for i in range(1, self.numPixels() - led_number):
                 self.setPixelColor(i, self._Green())
             for i in range(self.numPixels() - led_number, self.numPixels() - 1):
-                self.setPixelColor(i, self._Off())
+                self.setPixelColor(i, self._Blue())
         self.show()
