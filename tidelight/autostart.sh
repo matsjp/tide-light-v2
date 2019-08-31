97676d594820349653dca5094763185c9e4fe41b
matsjp <matspedersen@protonmail.com>
2019-08-31 21:08:25 +0200

    Added a shell script that makes the program auto run on boot

---
+++ tidelight/autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT=`realpath $0`
+echo $SCRIPT
+len=`expr length $SCRIPT`
+echo $len
+indexes=$(($len - 13))
+echo $indexes
+path=${SCRIPT:0:indexes }
+insertedcommand="sudo -- bash -c \"cd $path && nohup python3 main.py &\""
+echo $path
+echo $insertedcommand
+grepresult="$(grep -nr 'exit 0' /etc/rc.local | tail -1 | cut -d : -f 1)"
+echo $grepresult
+i="i"
+sedargument="$grepresult$i $insertedcommand"
+echo $sedargument
+sed -i "$sedargument"  /etc/rc.local
